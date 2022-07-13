38a4ceef4b5af7030dc563f8f872601e309a97a9
reveurguy <59417546+reveurguy@users.noreply.github.com>
2022-07-13 10:43:14 +0530

    avoid walls improved

--- src/bounds.js
+++ src/bounds.js
@@ -17,17 +17,27 @@
 function safeMoves(head, bounds) {
     let possibleMoves = [];
 
-    if (head.y === 0) {
-        possibleMoves.push('down') = false
+    // first hacky try to stay within bounds
+    if(head.x === 0) {
+        possibleMoves.push('right');
     }
-    if (head.y === bounds.height - 1) {
-        possibleMoves.push('up') = false
+    else if(head.x === bounds.width - 1) {
+        possibleMoves.push('left');
     }
-    if (head.x === bounds.width - 1) {
-        possibleMoves.push('right') = false
+    else {
+        possibleMoves.push('left');
+        possibleMoves.push('right');
     }
-    if (head.x === 0) {
-        possibleMoves.push('left') = false
+
+    if(head.y === 0) {
+        possibleMoves.push('up');
+    }
+    else if(head.y === bounds.height - 1) {
+        possibleMoves.push('down');
+    }
+    else {
+        possibleMoves.push('down');
+        possibleMoves.push('up');
     }
 
     return possibleMoves;
